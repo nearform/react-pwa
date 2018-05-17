608bdc4b30df206fe29f1a5b45602cfcd1f052aa
Florian Traverse <florian.traverse@rocksolead.com>
2018-05-17 09:52:02 +0200

    ci: add workflow, check lint and lighthouse
    Add @babel/polyfill on the server to be able to test lighthouse

--- scripts/lighthouse.js
+++ scripts/lighthouse.js
@@ -4,7 +4,7 @@ const lighthouse = require('lighthouse')
 const chromeLauncher = require('chrome-launcher')
 
 function launchChromeAndRunLighthouse (url, opts, config = null) {
-  return chromeLauncher.launch({chromeFlags: opts.chromeFlags}).then(chrome => {
+  return chromeLauncher.launch(opts).then(chrome => {
     opts.port = chrome.port
     return lighthouse(url, opts, config).then(results => {
       // The gathered artifacts are typically removed as they can be quite large (~50MB+)
@@ -15,13 +15,16 @@ function launchChromeAndRunLighthouse (url, opts, config = null) {
 }
 
 const opts = {
-  chromeFlags: ['--show-paint-rects']
+  chromeFlags: ['--show-paint-rects', '--headless', '--disable-gpu', '--no-sandbox'],
+  chromePath: process.env.LIGHTHOUSE_CHROMIUM_PATH,
+  logLevel: 'verbose',
+  port: 9222
 }
 
 server.init()
   .then(({ app, server }) => {
     console.log('\n\n server started on port 3000')
-    launchChromeAndRunLighthouse('http://localhost:3000/', opts).then(results => {
+    launchChromeAndRunLighthouse('http://127.0.0.1:3000/', opts).then(results => {
       console.log(results)
       server.close()
       process.exit()
