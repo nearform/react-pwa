eb8fa194419d23ed1a83c97a230b7b3d79c7eb65
Rod Melo <rodmelo@fromflux.com>
2018-06-06 13:16:54 +0100

    Add Jest and initial unit tests

---
+++ src/client/js/data/fetching.test.js
@@ -0,0 +1,52 @@
+const {fetchData} = require('./fetching')
+
+describe('fetchData', () => {
+  let mockedSuccess = true
+  let mockedPayload = {}
+  let mockedResponse = {
+    ok: mockedSuccess,
+    ...mockedPayload,
+    json: function() {
+      return mockedPayload
+    }
+  }
+
+  beforeEach(() => {
+    global.fetch = jest.fn().mockImplementation(() => {
+      return new Promise((resolve, reject) => {
+        resolve(mockedResponse)
+      })
+    })
+  })
+
+  it('returns response json', async () => {
+    mockedSuccess = true
+    mockedPayload = { foo: 'bar' }
+
+    await expect(fetchData()).resolves.toEqual(mockedPayload)
+  })
+
+  it('throws if response error', async () => {
+    mockedSuccess = false
+
+    try {
+      await expect(fetchData())
+    } catch (e) {
+      expect(e.code).toEqual('HTTP_ERROR')
+    }
+  })
+
+  it('calls api with appropriate url', async () => {
+    mockedSuccess = true
+    mockedPayload = { foo: 'bar' }
+
+    await fetchData()
+    expect(global.fetch).toHaveBeenCalledWith('/api/stories?filter=undefined&')
+
+    await fetchData('comments')
+    expect(global.fetch).toHaveBeenCalledWith('/api/comments?')
+
+    await fetchData('comments', 99)
+    expect(global.fetch).toHaveBeenCalledWith('/api/comments?page=99')
+  })
+})
