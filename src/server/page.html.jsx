6a9b2b37654da21a4b72381d4eab62aabdff90b8
Peter Hiža <peter.hiza@gmail.com>
2019-01-03 16:11:16 +0100

    Added needed app icons

--- src/server/page.html.jsx
+++ src/server/page.html.jsx
@@ -71,6 +71,10 @@ export async function renderPage(request, reply) {
         <meta name="theme-color" content={colors.NEARFORM_BRAND_MAIN} />
 
         <link rel="icon" href="/images/favicon.ico" sizes="32x32" />
+        <link rel="apple-touch-icon" href="/images/icon-120.png" />
+        <link rel="apple-touch-icon" href="/images/icon-152.png" sizes="152x152" />
+        <link rel="apple-touch-icon" href="/images/icon-167.png" sizes="167x167" />
+        <link rel="apple-touch-icon" href="/images/icon-180.png" sizes="180x180" />
         <link rel="shortcut icon" href="images/favicon.ico" sizes="196x196" />
         <link rel="manifest" href="/manifest.json" />
 
