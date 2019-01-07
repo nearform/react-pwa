9973d4d3b2cb16814da106c59867f9c3f3d47f31
Peter Hiža <peter.hiza@gmail.com>
2019-01-07 14:36:04 +0100

    Added app icons and splash screens (#70)

--- src/server/page.html.jsx
+++ src/server/page.html.jsx
@@ -74,11 +74,52 @@ export async function renderPage(request, reply) {
         <meta name="author" content="nearForm" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <meta name="apple-mobile-web-app-capable" content="yes" />
+        <meta name="apple-mobile-web-app-status-bar-style" content="default" />
         <meta name="theme-color" content={colors.NEARFORM_BRAND_MAIN} />
 
         <link rel="icon" href="/images/favicon.ico" sizes="32x32" />
         <link rel="shortcut icon" href="images/favicon.ico" sizes="196x196" />
+        <link rel="apple-touch-icon" href="/images/icon-120.png" />
+        <link rel="apple-touch-icon" href="/images/icon-152.png" sizes="152x152" />
+        <link rel="apple-touch-icon" href="/images/icon-167.png" sizes="167x167" />
+        <link rel="apple-touch-icon" href="/images/icon-180.png" sizes="180x180" />
         <link rel="manifest" href="/manifest.json" />
+
+        <link
+          rel="apple-touch-startup-image"
+          href="/images/splash_640x1136.png"
+          media="(device-width: 320px) and (device-height: 568px) and (-webkit-device-pixel-ratio: 2) and (orientation: portrait)"
+        />
+        <link
+          rel="apple-touch-startup-image"
+          href="/images/splash_750x1334.png"
+          media="(device-width: 375px) and (device-height: 667px) and (-webkit-device-pixel-ratio: 2) and (orientation: portrait)"
+        />
+        <link
+          rel="apple-touch-startup-image"
+          href="/images/splash_1242x2208.png"
+          media="(device-width: 414px) and (device-height: 736px) and (-webkit-device-pixel-ratio: 3) and (orientation: portrait)"
+        />
+        <link
+          rel="apple-touch-startup-image"
+          href="/images/splash_1125x2436.png"
+          media="(device-width: 375px) and (device-height: 812px) and (-webkit-device-pixel-ratio: 3) and (orientation: portrait)"
+        />
+        <link
+          rel="apple-touch-startup-image"
+          href="/images/splash_1536x2048.png"
+          media="(min-device-width: 768px) and (max-device-width: 1024px) and (-webkit-min-device-pixel-ratio: 2) and (orientation: portrait)"
+        />
+        <link
+          rel="apple-touch-startup-image"
+          href="/images/splash_1668x2224.png"
+          media="(min-device-width: 834px) and (max-device-width: 834px) and (-webkit-min-device-pixel-ratio: 2) and (orientation: portrait)"
+        />
+        <link
+          rel="apple-touch-startup-image"
+          href="/images/splash_2048x2732.png"
+          media="(min-device-width: 1024px) and (max-device-width: 1024px) and (-webkit-min-device-pixel-ratio: 2) and (orientation: portrait)"
+        />
 
         <style>{globalStyles.getStyles()}</style>
         <style>{getStyles()}</style>
